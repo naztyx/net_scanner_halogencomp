f6f621c39b260888f65d8dd901b7b21cecc4ccb1
qraxex <samuelpaulyila@gmail.com>
2023-01-16 17:09:43 +0100

    second commit

--- deauth_intruders.py
+++ deauth_intruders.py
@@ -1,35 +1,37 @@
-from scapy.all import RadioTap,sendp,Dot11,Dot11Deauth
-
-import argparse as ap
-
-def disconnect_intruders(t_mac, g_mac, intf=0.1, count=None, loop=1, interface='wlan0mon',verbose=0):
-
-    d11 = Dot11(addr=t_mac,addr2=g_mac,addr3=g_mac) #used to create 802.11 frames
-
-    packet_stack = RadioTap()/d11/Dot11Deauth(reason=7)
-     
-    sendp(packet_stack,intf=intf,count=count,loop=loop,interface=interface,verbose=verbose)
+# 
 
 
-if __name__=='__main__':
-    parser = ap.ArgumentParser(description='Deauthenticating intruders...')
-    parser.add_argument('target', help='Unauthorized Target Mac Address.')
-    parser.add_argument('gateway', help='Mac Address of Router/Gateway')
-    parser.add_argument('-c', '--count', help='Number of deauth frames to send between 0-infinity.')
-    parser.add_argument('--interval', help='Sending frequency')
-    parser.add_argument('-i', dest='iface', help='interface to use while in monitor mode')
+######
 
-    args = parser.parse_args()
-    target = args.target
-    gateway = args.gateway
-    count = int(args.count)
-    interval = float(args.interval)
-    iface =args.iface
 
-    if count ==0:
-        loop = 1
-        count=None
-    else:
-        loop = 1
+# from scapy.all import conf, getmacbyip, Dot11, Dot11Deauth, RadioTap, sendp
 
-    disconnect_intruders('192.168.0.160',gateway,interval,count,loop,iface)
+# import argparse
+
+# # print(get_if_addr(conf.iface))
+# # print(conf.route.route('0.0.0.0')[2])
+# # print(getmacbyip(conf.route.route('0.0.0.0')[2]))
+
+# def deeauth(target, gateway):
+#     target = getmacbyip(conf.route.route(target)[2])
+#     gateway = getmacbyip(conf.route.route(gateway)[2])
+
+#     #create deauthentication packets
+#     dot11 = Dot11(type=0, subtype=12, addr1=target, addr2=gateway, addr3=gateway)
+#     packet = RadioTap()/dot11/Dot11Deauth(reason=7)
+
+#     #send the packets
+#     sendp(packet, inter=0.1, count=1000, verbose=1)
+
+
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser('Python Script to De-authorized Connected Devices.')
+#     parser.add_argument('target_ip', help='Target Ip to scan.')
+#     parser.add_argument('gateway', help='Target gateway or router')
+
+#     args =parser.parse_args()
+
+#     target = args.target_ip
+#     gateway = args.gateway
+
+#     deeauth(target,gateway)
--- net_scanner.py
+++ net_scanner.py
@@ -1,32 +1,66 @@
 from scapy.all import ARP
 from scapy.all import Ether
 from scapy.all import srp
-import socket
+import argparse
 
-target_net = '192.168.0.1/24'
 
-#arp packets
-arp = ARP(pdst=target_net)
+def unauthorized_devices_filter(ip_to_scan):
 
-#create packet to be broadcast by Ether
-eth = Ether(dst='ff:ff:ff:ff:ff:ff')
+    ## set the ip address of the router or gateway device with the notation /24 or /16 depending
+    ## on the ip range you wish to scan
+    target_net = ip_to_scan + '/24'
 
-packet_stack = eth/arp
+    #arp packets
+    arp = ARP(pdst=target_net)
 
-output = srp(packet_stack,timeout=2,verbose=0)[0] #set verbose to 0 to reduce unnecessary print out on screen
+    #create packet to be broadcast by Ether
+    eth = Ether(dst='ff:ff:ff:ff:ff:ff')
 
-#create an empty list of clients to be populated later
-list_of_users_on_network = []
+    packet_stack = eth/arp
 
-for sent_packets, received_packets in output:
-    #append each mac to corresponding ip
-    list_of_users_on_network.append({'mac_address': received_packets.hwsrc,
-                                    'ip_address': received_packets.psrc})
+    #srp is used tro send out the packets
+    #set verbose to 0 to reduce unnecessary print out on screen,
+    #set timeout to 2 to prevent the script from freezing or malfunctioning
+    output = srp(packet_stack,timeout=2,verbose=0)[0] 
 
-#print list of connected devices on the network
-print('>>> Printing list of connected devices on the network: \n')
+    #create an empty list of clients (ip and mac) to be populated later from the output
+    list_of_connected_devices = []
 
-print('>>> '+'Ip'+ ' '*18 + 'Mac')
-for users in list_of_users_on_network:
-    print('>>> {:16}    {}'.format(users['ip_address'], users['mac_address']))
+    for sent_packets, received_packets in output:
+        #print(received_packets.psrc)
 
+        #append each mac to corresponding ip
+        list_of_connected_devices.append({'mac_address': received_packets.hwsrc,
+                                        'ip_address': received_packets.psrc})
+
+    ## read a list of whitleisted mac adresses and create an empty filtered list
+    mac_whitelist = open('whitelist.txt').readlines()
+    clean_macs = []
+    ## strip newline \n from the mac strings
+    for i in mac_whitelist:
+        clean_macs.append(i.rstrip())
+    #print(clean_macs)
+
+    ## SORT AND PRINT LIST OF ALL CONNECTED DEVICES
+    print('>>> List of Connected Devices...')
+    print('>>> '+'Ip'+ ' '*18 + 'Mac')
+    for device in list_of_connected_devices:
+        print('>>> {:16}    {}'.format(device['ip_address'], device['mac_address']))
+
+    ## SORT AND PRINT LIST OF UNAUTHORIZED CONNECTIONS
+    print('>>> \nList of Unauthorized Connected Devices...')
+    print('>>> '+'Ip'+ ' '*18 + 'Mac')
+    for device in list_of_connected_devices:
+        if device['mac_address'] in clean_macs:
+            return
+        else:
+            print('>>> {:16}    {}'.format(device['ip_address'], device['mac_address']))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Python Script to Scan and Filter Unauthorized Connected Devices.')
+    parser.add_argument('target_ip', help='Target Ip to scan.')
+
+    args =parser.parse_args()
+    target = args.target_ip
+
+    unauthorized_devices_filter(target)
